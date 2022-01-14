e5b98b060c68dd76de3367de96a17453290158c8
iamleahb <leah.ball@gmail.com>
2022-01-14 11:25:03 -0600

    finished questions on MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
+import MutationObserver from "mutationobserver-shim";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
