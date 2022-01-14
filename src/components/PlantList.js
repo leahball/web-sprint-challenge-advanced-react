8b57604c7ee1b6b9b12ad44e43136aed289d4be0
iamleahb <leah.ball@gmail.com>
2022-01-14 12:23:18 -0600

    tests passing but plantlist doesn't render to screnn

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,12 +4,19 @@ import axios from "axios";
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
-    plants:[]
-  }
+    plants: [],
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
+  componentDidMount() {
+    axios.get("http://localhost:3333/plants").then((resp) => {
+      this.setState({
+        ...this.state.plants,
+        plants: resp.data,
+      });
+    });
+  }
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
